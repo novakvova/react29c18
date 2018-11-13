64b1eb16a484d405dccaa075d2de6b2063c33cca
novakvova <novakvova@gmail.com>
2018-11-13 19:13:04 +0200

    Add style css

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ArticleList from './ArticleList';
+import ArticleList from './ArticleList/index.js';
 import articles from '../data';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
     return (
-        <div>
-            <h1>My app Hello!</h1>
-            <ArticleList articles = {articles} />
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-3">My app Hello!</h1>
+            </div>
+            <ArticleList articles={articles} />
         </div>
     )
 }
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Article from './Article'
+import React from 'react';
+import Article from '../Article';
+import './style.css';
 
 export default function ArticleList({articles}) {
     const articleElements = articles.map(artItem =>
-        <li key={artItem.id}><Article article={artItem}/></li>
+        <li key={artItem.id} className="article-list__li"><Article article={artItem}/></li>
         );
     return (
-        <ul>
+        <ul style={{paddingLeft: 0}}>
             {articleElements}
             {/* <li><Article article={articles[0]}/></li>
             <li><Article article={articles[1]}/></li> */}
